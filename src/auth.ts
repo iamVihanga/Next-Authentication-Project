9a2c6ea156a45c48f41bbf70fa3f513da059bc00
Vihanga Rashan Silva <vihangarashansilva@gmail.com>
2024-09-30 16:12:44 +0530

    Add: Integrate Auth Action Callbacks

--- src/auth.ts
+++ src/auth.ts
@@ -2,8 +2,45 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "@/auth.config";
 import { db } from "@/db/db";
+import { getUserById } from "@/db/functions";
+import { UserRole } from "@prisma/client";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  callbacks: {
+    // async signIn({ user }) {
+    //   if (!user) return false;
+
+    //   const exsistingUser = await getUserById(user.id as string);
+
+    //   if (!exsistingUser || !exsistingUser.emailVerified) return false;
+
+    //   return true;
+    // },
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      // Fetch exsisting user
+      const exsistingUser = await getUserById(token.sub);
+
+      if (!exsistingUser) return token;
+
+      // Bind user role to token
+      token.role = exsistingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
